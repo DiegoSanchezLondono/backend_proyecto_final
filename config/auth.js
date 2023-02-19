edf2597f6be68bdfbc66384bd4a0393f60677272
Diego Sánchez <diegosanchezlondono@gmail.com>
2023-02-19 17:47:01 +0100

    se agrega URL de mongodb

--- config/auth.js
+++ config/auth.js
@@ -1,1 +1,10 @@
 
+module.exports = {
+
+    SECRET: process.env.AUTH_SECRET || "pingüino", 
+    EXPIRES: process.env.AUTH_EXPIRES || "24h",
+    ROUNDS : process.env.AUTH_ROUNDS || 10,
+    PORT : process.env.PORT || 5500,
+    URL : process.env.URL || `mongodb+srv://diego:TY0chgHp8Z1fDeH8@cluster0.xipat0i.mongodb.net/integrarTEA?retryWrites=true&w=majority`
+
+};
