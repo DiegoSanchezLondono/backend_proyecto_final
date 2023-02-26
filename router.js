634b6dc993d36de0fc3ed5e28dcd99306fc335f4
Diego Sánchez <diegosanchezlondono@gmail.com>
2023-02-26 13:44:29 +0100

    se agrega ruta de favorites en router.js

--- router.js
+++ router.js
@@ -6,11 +6,13 @@ const UsersRouter = require('./views/UsersRouter');
 const RolesRouter = require('./views/RolesRouter');
 const PictogramsRouter = require('./views/PictogramsRouter');
 const VideosRouter = require('./views/VideosRouter');
+const FavoritesRouter = require('./views/FavoritesRouter');
 
 router.use("/users", UsersRouter);
 router.use("/roles", RolesRouter);
 router.use("/pictograms", PictogramsRouter);
 router.use("/videos", VideosRouter);
+router.use("/favorites", FavoritesRouter);
 
 //Exporto router
 module.exports = router;
