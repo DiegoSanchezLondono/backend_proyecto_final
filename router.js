c85b17faacad4beeb4f68dffcac55ece7666a2ec
Diego Sánchez <diegosanchezlondono@gmail.com>
2023-02-26 13:42:30 +0100

    se añade funcionalidad a Favorites

--- router.js
+++ router.js
@@ -4,9 +4,13 @@ const router = require ('express').Router();
 
 const UsersRouter = require('./views/UsersRouter');
 const RolesRouter = require('./views/RolesRouter');
+const PictogramsRouter = require('./views/PictogramsRouter');
+const VideosRouter = require('./views/VideosRouter');
 
 router.use("/users", UsersRouter);
 router.use("/roles", RolesRouter);
+router.use("/pictograms", PictogramsRouter);
+router.use("/videos", VideosRouter);
 
 //Exporto router
 module.exports = router;
